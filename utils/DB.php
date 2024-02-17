621bdebd3fe536391835d6520ef1ae3842ded3ba
maorbenzvi <maor@welexit.be>
2024-02-17 13:36:32 +0100

    Add try and catch to DB connection in case of error + add comments

--- utils/DB.php
+++ utils/DB.php
@@ -4,9 +4,24 @@ class DB
 {
 	private $pdo;
 
+    /**
+     * Constructor for the DB class.
+     * Initializes a new PDO connection using the provided credentials.
+     * Previously an instantiate method was used, which is not the correct way to instantiate a class
+     *
+     * @param string $dsn The Data Source Name, or DSN, containing the information required to connect to the database.
+     * @param string $user The username for the DSN string.
+     * @param string $password The password for the DSN string.
+     */
 	public function __construct($dsn, $user, $password)
 	{
-		$this->pdo = new \PDO($dsn, $user, $password);
+        try{
+            $this->pdo = new PDO($dsn, $user, $password, [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Error handling strategy
+            ]);
+        } catch (PDOException $e) {
+            die('Connection failed: ' . $e->getMessage());
+        }
 	}
 
     public function select($sql, $params = [])
