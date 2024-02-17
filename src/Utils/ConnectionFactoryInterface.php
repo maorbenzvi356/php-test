3a0f1d30d9cb96f9d476476059909aeaec0037c7
maorbenzvi <maor@welexit.be>
2024-02-17 17:58:29 +0100

    Make added classes psr compliant

--- src/Utils/ConnectionFactoryInterface.php
+++ src/Utils/ConnectionFactoryInterface.php
@@ -1,7 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Utils;
 
-interface ConnectionFactoryInterface {
+interface ConnectionFactoryInterface
+{
     public function createConnection(): DB;
 }
